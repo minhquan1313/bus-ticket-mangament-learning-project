a1937a594efd00a9daaf85b25b74736aa3f4c8a0
minhquan1313 <minhquan1313@gmail.com>
2022-11-21 20:31:15 +0700

    update

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -8,7 +8,25 @@
             <div class="absolute bottom-0 inset-x-0 bg-gradient-to-t from-white to-white/0 top-1/2"></div>
         </div>
 
-        <div class="absolute bottom-5 left-1/2 -translate-x-1/2 rounded-2xl bg-oWhite px-5 py-3">hi</div>
+        <form action="javascript:;" method="GET"
+            class="absolute bottom-5 left-1/2 -translate-x-1/2 flex items-center rounded-xl bg-oWhite px-4 p-3 text-sm">
+
+            {{-- <div class="rounded-xl overflow-hidden divide-x"></div> --}}
+            <div class="flex rounded-xl overflow-hidden divide-x">
+                <label class="bg-red-400 flex items-center">
+                    <span class="material-symbols-outlined">
+                        travel_explore
+                    </span>
+
+                    <input type="text" class="">
+                </label>
+            </div>
+
+            <div class="rounded-xl overflow-hidden divide-x">
+                <button class="px-5 py-2 bg-oYellow text-oWhite" type="submit">Tim kiem</button>
+            </div>
+
+        </form>
 
     </div>
 @endsection
